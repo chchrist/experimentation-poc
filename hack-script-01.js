f8631bac94147f8a8caa0ca841324be8e779d866
Tolis <tolis.christomanos@gamesys.co.uk>
2020-03-16 15:26:57 +0000

    changes

--- hack-script-01.js
+++ hack-script-01.js
@@ -20,4 +20,22 @@ window.onload = function() {
   ); // Noncompliant
 
   document.querySelector("body").innerHTML = '<a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>';
+
+  var myWindow = document.getElementById('myIFrame').contentWindow;
+myWindow.postMessage(message, "*"); // Noncompliant; how do you know what you loaded in 'myIFrame' is still there?
+
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(
+{
+  host:'localhost',
+  user: "admin",
+  database: "project",
+  password: "mypassword", // sensitive
+  multipleStatements: true
+});
+
+connection.connect();
+
 };
