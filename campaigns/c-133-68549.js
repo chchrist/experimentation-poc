5596889bea27806c0913802d228f5aa8b1e8093b
Tolis <tolis.christomanos@gamesys.co.uk>
2020-03-17 16:06:05 +0000

    adding campaign script

---
+++ campaigns/c-133-68549.js
@@ -0,0 +1,10 @@
+<style>
+</style>
+<script>
+
+dom.changeImageBanner("#null-link > img", {
+	imageUrl: "//service.maxymiser.net/cm/images-eu/1/1/1/60A8E50681E70E19E3A84172F17F8F90795839E8337CF61780301F61C81AA04F/jackpotjoy-com/JPJ-Welcome-Offer-Banner/test.JPG"
+});
+dom.resize("#null-link > img", 644, 6, 0, 0);
+//UCB JS Placeholder//
+</script>
